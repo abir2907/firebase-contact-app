33cc4ac16e5767b3d9e963bbc08e98afb79e8725
Abir Chodha <abirchodha1@gmail.com>
2025-01-17 02:11:17 +0530

    Create SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 const App = () => {
   return (
-    <div className="mx-auto max-w-[370px]">
+    <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
+      <div className="flex gap-2">
+        <div className="relative flex flex-grow items-center">
+          <FiSearch className="absolute ml-1 text-3xl text-white" />
+          <input
+            type="text"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+          />
+        </div>
+
+        <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
+      </div>
     </div>
   );
 };
