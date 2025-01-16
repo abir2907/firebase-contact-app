f2d1fc5719e624e416d2bd395bf138bd27205c5a
Abir Chodha <abirchodha1@gmail.com>
2025-01-17 03:52:04 +0530

    Create Modal component using portal

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import { AiFillPlusCircle } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
+import ContactCard from "./components/ContactCard";
+import Modal from "./components/Modal";
+import AddAndUpdateContact from "./components/AddAndUpdateContact";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
+
+  const [isOpen, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
 
   useEffect(() => {
     const getContacts = async () => {
@@ -16,7 +24,7 @@ const App = () => {
         const contactLists = contactsSnapshot.docs.map((doc) => {
           return { id: doc.id, ...doc.data() };
         });
-        console.log(contactLists);
+        setContacts(contactLists);
       } catch (error) {
         console.error(error);
       }
@@ -26,20 +34,32 @@ const App = () => {
   }, []);
 
   return (
-    <div className="mx-auto max-w-[370px] px-4">
-      <Navbar />
-      <div className="flex gap-2">
-        <div className="relative flex flex-grow items-center">
-          <FiSearch className="absolute ml-1 text-3xl text-white" />
-          <input
-            type="text"
-            className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+    <>
+      <div className="mx-auto max-w-[370px] px-4">
+        <Navbar />
+        <div className="flex gap-2">
+          <div className="relative flex flex-grow items-center">
+            <FiSearch className="absolute ml-1 text-3xl text-white" />
+            <input
+              type="text"
+              className="h-10 flex-grow rounded-md border border-white bg-transparent pl-9 text-white"
+            />
+          </div>
+
+          <AiFillPlusCircle
+            onClick={onOpen}
+            className="cursor-pointer text-5xl text-white"
           />
         </div>
 
-        <AiFillPlusCircle className="cursor-pointer text-5xl text-white" />
+        <div className="mt-4 flex flex-col gap-3">
+          {contacts.map((contact) => (
+            <ContactCard key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
-    </div>
+      <AddAndUpdateContact isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
---
+++ src/components/AddAndUpdateContact.jsx
@@ -0,0 +1,13 @@
+import Modal from "./Modal";
+
+const AddAndUpdateContact = ({ isOpen, onClose }) => {
+  return (
+    <div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        HI
+      </Modal>
+    </div>
+  );
+};
+
+export default AddAndUpdateContact;
