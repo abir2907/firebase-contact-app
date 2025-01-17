d3722dab75ff4b1f9b0570ab7102b12dd30b4aba
Abir Chodha <abirchodha1@gmail.com>
2025-01-17 22:19:18 +0530

    Create form in modal using formik

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -1,10 +1,26 @@
+import { Field, Form, Formik } from "formik";
 import Modal from "./Modal";
 
 const AddAndUpdateContact = ({ isOpen, onClose }) => {
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
-        HI
+        <Formik>
+          <Form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name</label>
+              <Field name="name" className="h-10 border" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <Field name="email" className="h-10 border" />
+            </div>
+
+            <button className="self-end border bg-orange px-3 py-1.5">
+              Add Contact
+            </button>
+          </Form>
+        </Formik>
       </Modal>
     </div>
   );
