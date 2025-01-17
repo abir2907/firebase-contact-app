d7fb33f63ad675a648625fa9acc94d97f18cc1ec
Abir Chodha <abirchodha1@gmail.com>
2025-01-17 22:46:29 +0530

    Add delete contact functionality

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,18 +1,29 @@
 /* eslint-disable react/prop-types */
 
+import { collection, deleteDoc } from "firebase/firestore";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { IoMdTrash } from "react-icons/io";
 import { RiEditCircleLine } from "react-icons/ri";
+import { db } from "../config/firebase";
+import { doc } from "firebase/firestore";
 
 const ContactCard = ({ contact }) => {
+  const deleteContact = async (id) => {
+    try {
+      await deleteDoc(doc(db, "contacts", id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div
         key={contact.id}
-        className="bg-yellow flex items-center justify-between rounded-lg p-2"
+        className="flex items-center justify-between rounded-lg bg-yellow p-2"
       >
         <div className="flex gap-1">
-          <HiOutlineUserCircle className="text-orange text-4xl" />
+          <HiOutlineUserCircle className="text-4xl text-orange" />
           <div className="">
             <h2 className="font-medium">{contact.name}</h2>
             <p className="text-sm">{contact.email}</p>
@@ -20,7 +31,10 @@ const ContactCard = ({ contact }) => {
         </div>
         <div className="flex text-3xl">
           <RiEditCircleLine className="cursor-pointer" />
-          <IoMdTrash className="text-orange cursor-pointer" />
+          <IoMdTrash
+            onClick={() => deleteContact(contact.id)}
+            className="cursor-pointer text-orange"
+          />
         </div>
       </div>
     </>
