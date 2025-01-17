74950450bedb822649438d29b265afa26ffb23f2
Abir Chodha <abirchodha1@gmail.com>
2025-01-17 22:25:35 +0530

    Prevent modal from closing when clicking on the overlay

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,10 @@ const Modal = ({ onClose, isOpen, children }) => {
           onClick={onClose}
           className="absolute top-0 z-40 grid h-screen w-screen place-items-center backdrop-blur"
         >
-          <div className="relative z-50 m-auto min-h-[200px] min-w-[80%] bg-white p-4">
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="relative z-50 m-auto min-h-[200px] min-w-[80%] bg-white p-4"
+          >
             <div className="flex justify-end">
               <AiOutlineClose
                 onClick={onClose}
